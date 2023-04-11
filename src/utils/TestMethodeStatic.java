1780ddad241e3bf22a7cebe76ae929b8b2eaafde
Driss Gazzaoui <drissgazzaoui1986@hotmail.com>
2023-04-12 00:11:23 +0200

    TP 4

--- src/utils/TestMethodeStatic.java
+++ src/utils/TestMethodeStatic.java
@@ -6,8 +6,19 @@ public class TestMethodeStatic {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String chaine = "12";
+		int resultat = Integer.parseInt(chaine);
 		
-		System.out.println("la chaine en int : " + Integer.parseInt(chaine));
+		System.out.println("la chaine en int : " + resultat );		
+		
+		int a = 23;
+		int b = 4;
+		int max = Integer.max(a, b);
+
+        System.out.println("Le nombre maximum entre " + a + " et " + b + " est " + max);
+		
+		
+		
+		
 
 	}
 
