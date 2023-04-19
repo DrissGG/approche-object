8d4a160706aacd12676a77985400a03d4daa3e9d
Driss Gazzaoui <drissgazzaoui1986@hotmail.com>
2023-04-19 13:06:52 +0200

    TP Dates

---
+++ src/fr/diginamic/dates/TestCalendar.java
@@ -0,0 +1,34 @@
+package fr.diginamic.dates;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+public class TestCalendar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Calendar calendrier = Calendar.getInstance();
+		calendrier.set(Calendar.DAY_OF_MONTH, 19);
+		calendrier.set(Calendar.MONTH, 4);
+		calendrier.set(Calendar.YEAR, 2016);
+		calendrier.set(Calendar.HOUR_OF_DAY, 23);
+		calendrier.set(Calendar.MINUTE, 59);
+		calendrier.set(Calendar.SECOND, 30);
+		
+		Date date = calendrier.getTime();
+		SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyy ");
+		System.out.println("date  : "+ format.format(date));
+		
+		SimpleDateFormat format2 = new SimpleDateFormat("yyy/MM/dd  HH:mm:ss");
+		System.out.println("date  : "+ format2.format(date));
+		
+		SimpleDateFormat formatFr = new SimpleDateFormat("EEEEE dd MMMMM yyyy", Locale.FRANCE);
+		System.out.println("date en FR  : "+ formatFr.format(date));
+		
+		SimpleDateFormat formatRusse = new SimpleDateFormat("EEEEE dd MMMMM yyyy", new Locale("ru"));		
+		System.out.println("date en Russe : "+ formatRusse.format(date));
+		
+		SimpleDateFormat formatChinois = new SimpleDateFormat("EEEEE dd MMMMM yyyy", Locale.CHINA);		
+		System.out.println("date en Chinois : "+ formatChinois.format(date));
+	}
+
+}
---
+++ src/fr/diginamic/dates/TestDates.java
@@ -0,0 +1,22 @@
+package fr.diginamic.dates;
+
+import java.util.*;
+import java.text.SimpleDateFormat;
+
+public class TestDates {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SimpleDateFormat formateur = new SimpleDateFormat("dd/MM/yyyy ");
+		Date date = new Date();
+		System.out.println("Date du jour : " + formateur.format(date));
+		
+		Date date1 = new Date(116, 4, 19, 23, 59, 30);
+		SimpleDateFormat formateur1 = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+		System.out.println("Date spécifiée : " + formateur1.format(date1));
+
+		Date date3 = new Date();
+		System.out.println("Date du systeme : " +(formateur1.format(date3)));
+	}
+
+}
